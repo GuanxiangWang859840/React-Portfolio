b843ed76993780cd6c51e92a5174e87dfb45a03a
Guanxiang Wang <gn218717@dal.ca>
2025-03-14 23:22:58 -0300

    doe

---
+++ Desktop/untitled folder 3/My-Portfolio/src/pages/About.jsx
@@ -0,0 +1,14 @@
+export default function About() {
+    return (
+      <div className="container">
+        <h1>About Me</h1>
+        <p>I am a Computer Science student at Dalhousie University with a passion for technology and software development.</p>
+        <h2>Skills</h2>
+        <ul>
+          <li>Programming Languages: C, Java, Python, PHP, HTML/CSS/JavaScript, SQL, R</li>
+          <li>Frameworks: React.js, Flask, Bootstrap</li>
+          <li>Tools: Git, Docker, Jira, Confluence, Linux</li>
+        </ul>
+      </div>
+    );
+  }
