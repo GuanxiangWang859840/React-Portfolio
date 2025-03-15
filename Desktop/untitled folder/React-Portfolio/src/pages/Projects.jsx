01273578ffbd1f0ab609b89a1d18edd831f3349c
Guanxiang Wang <gn218717@dal.ca>
2025-03-14 23:25:05 -0300

    done

---
+++ Desktop/untitled folder/React-Portfolio/src/pages/Projects.jsx
@@ -0,0 +1,13 @@
+export default function Projects() {
+    return (
+      <div className="container">
+        <h1>Projects</h1>
+        <p>Here are some of my projects:</p>
+        <ul>
+          <li>Java-Based Game Development</li>
+          <li>Web Development - fementity.ca</li>
+          <li>Android Application for Task Hiring</li>
+        </ul>
+      </div>
+    );
+  }
